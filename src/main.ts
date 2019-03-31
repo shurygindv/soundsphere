548b0553152bcd28a0041233a0bb453b844f83a5
shurygindv <shurygindv1@gmail.com>
2019-03-31 17:41:04 +0400

    Server: fixes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import 'graphql-import-node';
 
-import { AppModule } from './modules/app.module';
+import { AppModule } from './graphql/graphql.module';
 import { bootstrap } from './bootstrapper';
 
 bootstrap(AppModule);
