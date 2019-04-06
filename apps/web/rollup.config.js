bb16341ed4add6743f0f4c2d7f0c664de7fc259f
shurygindv <shurygindv1@gmail.com>
2019-04-07 00:42:41 +0400

    Web: added serve / html entry point

--- apps/web/rollup.config.js
+++ apps/web/rollup.config.js
@@ -1,4 +1,6 @@
 import bucklescript from 'rollup-plugin-bucklescript';
+import html from 'rollup-plugin-bundle-html';
+import serve from 'rollup-plugin-serve';
 
 export default {
   input: './src/Index.re',
@@ -10,6 +12,17 @@ export default {
     bucklescript({
       module: 'es6',
       showWarnings: true
+    }),
+    html({
+      template: 'public/index.html',
+      dest: "dist",
+      filename: 'index.html',
+      inject: 'body',
+    }),
+    serve({
+      open: true,
+      contentBase: 'dist',
+      port: 7771
     })
   ]
 };
